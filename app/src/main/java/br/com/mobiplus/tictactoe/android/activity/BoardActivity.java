a8615971ae6c5bb345812506b826a3d288f505da
Luis E. Fernandez <luis.fernandez@Luis-Fernandezs-iMac.local>
2016-03-16 23:11:22 -0300

    Deleting AdMob dependency

--- app/src/main/java/br/com/mobiplus/tictactoe/android/activity/BoardActivity.java
+++ app/src/main/java/br/com/mobiplus/tictactoe/android/activity/BoardActivity.java
@@ -1,23 +1,15 @@
 package br.com.mobiplus.tictactoe.android.activity;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import br.com.mobiplus.tictactoe.R;
 import br.com.mobiplus.tictactoe.mvp.presenter.BoardPresenter;
 import br.com.mobiplus.tictactoe.mvp.presenter.IBoardPresenter;
 
 public class BoardActivity extends AppCompatActivity {
-    // Remove the below line after defining your own ad unit ID.
-    private static final String TOAST_TEXT = "Test ads are being shown. "
-            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
-
 
     private IBoardPresenter mPresenter;
 
@@ -27,15 +19,6 @@ public class BoardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_board);
 
         mPresenter = new BoardPresenter(BoardActivity.this);
-
-        // Load an ad into the AdMob banner view.
-        AdView adView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder()
-                .setRequestAgent("android_studio:ad_template").build();
-        adView.loadAd(adRequest);
-
-        // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
-        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -49,26 +32,4 @@ public class BoardActivity extends AppCompatActivity {
         super.onStop();
         mPresenter.onStop();
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_board, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 }
