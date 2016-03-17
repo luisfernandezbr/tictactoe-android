f9c69a35a65e242c470d5e30abb0b7ce0d9df099
Luis E. Fernandez <luis.fernandez@Luis-Fernandezs-iMac.local>
2016-03-16 22:52:15 -0300

    Adding BaseView class

---
+++ app/src/main/java/br/com/mobiplus/tictactoe/mvp/view/BaseView.java
@@ -0,0 +1,36 @@
+package br.com.mobiplus.tictactoe.mvp.view;
+
+import android.app.Activity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by luis.fernandez on 3/16/16.
+ */
+public abstract class BaseView {
+
+    protected Activity mActivity;
+    protected ViewGroup mRootView;
+
+    public BaseView(Activity activity) {
+        this.mActivity = activity;
+        this.initDefaultViews(this.getLayoutResId());
+        this.initViews();
+    }
+
+    private void initDefaultViews(int layoutResId) {
+        mActivity.setContentView(layoutResId);
+        mRootView = (ViewGroup) mActivity.getWindow().getDecorView().findViewById(android.R.id.content);
+    }
+
+    protected View findViewById(int resId) {
+        return mActivity.findViewById(resId);
+    }
+
+    protected abstract int getLayoutResId();
+
+    protected abstract void initViews();
+}
