8ec376c28f0d4ca662a008860d2d861a7cbaedd5
Luis E. Fernandez <luis.fernandez@Luis-Fernandezs-iMac.local>
2016-03-16 23:07:40 -0300

    Adding Application class

---
+++ app/src/main/java/br/com/mobiplus/tictactoe/android/application/AppApplication.java
@@ -0,0 +1,14 @@
+package br.com.mobiplus.tictactoe.android.application;
+
+import android.app.Application;
+
+/**
+ * Created by luis.fernandez on 3/16/16.
+ */
+public class AppApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
