8d3b85b7f62dbc59d713f25d03b7099e8a43ed43
Luis E. Fernandez <luis.fernandez@Luis-Fernandezs-iMac.local>
2016-03-20 15:58:45 -0300

    Deleting unused code

--- app/src/main/java/br/com/mobiplus/tictactoe/ai/chain/AbstractBestPlayChooser.java
+++ app/src/main/java/br/com/mobiplus/tictactoe/ai/chain/AbstractBestPlayChooser.java
@@ -1,9 +1,6 @@
 package br.com.mobiplus.tictactoe.ai.chain;
 
-import java.util.List;
-
 import br.com.mobiplus.tictactoe.pojo.Board;
-import br.com.mobiplus.tictactoe.pojo.BoardCell;
 
 /**
  * Created by luis.fernandez on 3/19/16.
@@ -20,26 +17,5 @@ public abstract class AbstractBestPlayChooser {
 
     public void setNextInChain(AbstractBestPlayChooser next) {
         this.mNext = next;
-    }
-
-    protected boolean hasTwoCellsOccupied(List<BoardCell> boardCellList, String cellValue) {
-        boolean result = false;
-
-        int count = 0;
-
-        for (int i = 0; i < boardCellList.size(); i++) {
-            BoardCell boardCell = boardCellList.get(i);
-
-            if (cellValue.equals(boardCell.getValue())) {
-                count++;
-
-                if (count == 2) {
-                    result = true;
-                    break;
-                }
-            }
-        }
-
-        return result;
     }
 }
