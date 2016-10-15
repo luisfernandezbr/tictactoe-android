3e67e7d8f5852c82b65d005435f84f2eafa4c92a
Carlos Gama <gamaroot@gmail.com>
2016-10-15 13:22:04 -0300

    AND_ENGINE - Criada entidade que mantêm operações referentes às cenas de jogo.

---
+++ app/src/main/java/br/com/mobiplus/tictactoe/and_engine/game/GameScreen.java
@@ -0,0 +1,75 @@
+package br.com.mobiplus.tictactoe.and_engine.game;
+
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.entity.IEntity;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.RepeatingSpriteBackground;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.opengl.texture.TextureManager;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.source.AssetBitmapTextureAtlasSource;
+
+import br.com.mobiplus.tictactoe.R;
+import br.com.mobiplus.tictactoe.android.ContextLoader;
+
+/**
+ * Created by Gama on 15/10/2016.
+ */
+public class GameScreen {
+    private final int CAMERA_WIDTH  = 768;
+    private final int CAMERA_HEIGHT = 1280;
+
+    private ContextLoader contextLoader;
+    private Scene currentScene;
+    private RepeatingSpriteBackground sceneBackground;
+
+    public GameScreen(ContextLoader pContextLoader) {
+        contextLoader = pContextLoader;
+    }
+
+    public Camera setupCamera() {
+        return new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    }
+
+    public Scene setupScene() {
+        currentScene = new Scene();
+        currentScene.setTouchAreaBindingEnabled(true);
+        currentScene.setBackground(sceneBackground);
+        return currentScene;
+    }
+
+    public void setupSceneBackground(TextureManager pTextureManager) {
+        sceneBackground = new RepeatingSpriteBackground(CAMERA_WIDTH, CAMERA_HEIGHT,
+                pTextureManager, new AssetBitmapTextureAtlasSource(contextLoader.loadContext(), contextLoader.loadContext().getString(R.string.main_scene_background)));
+    }
+
+    public void sortEntities() {
+        currentScene.sortChildren();
+    }
+
+    private void putEntityOnTop(IEntity pEntity) {
+        if (pEntity != null) {
+            pEntity.setZIndex(currentScene.getChildCount() + 1);
+        }
+    }
+
+    public void addEntity(IEntity pEntity) {
+        if (pEntity != null) {
+            currentScene.attachChild(pEntity);
+            pEntity.setZIndex(currentScene.getChildCount() + 1);
+        }
+    }
+
+    public void addEntities(IEntity ... pEntities) {
+        for (IEntity entity : pEntities) {
+            addEntity(entity);
+        }
+    }
+
+    public void registerTouchArea(Sprite pSprite) {
+        currentScene.registerTouchArea(pSprite);
+    }
+
+    public void unregisterTouchArea(Sprite pSprite) {
+        currentScene.unregisterTouchArea(pSprite);
+    }
+}
