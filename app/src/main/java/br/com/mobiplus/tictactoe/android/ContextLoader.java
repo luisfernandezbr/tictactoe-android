f70da437be2bcf4b292c4ff66454776a60d08e96
Carlos Gama <gamaroot@gmail.com>
2016-10-15 13:15:06 -0300

    AND_ENGINE - Criada interface para carregamento de contexto.

---
+++ app/src/main/java/br/com/mobiplus/tictactoe/android/ContextLoader.java
@@ -0,0 +1,10 @@
+package br.com.mobiplus.tictactoe.android;
+
+import android.content.Context;
+
+/**
+ * Created by Gama on 15/10/2016.
+ */
+public interface ContextLoader {
+    Context loadContext();
+}
